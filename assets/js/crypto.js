47a16bdce8e6f938f6ad327cceae39293297b102
Alfonso Cerv <pedrocerv1@hotmail.com>
2021-04-27 17:40:33 -0500

    mental reset

--- assets/js/crypto.js
+++ assets/js/crypto.js
@@ -1,35 +1,18 @@
 fetch("https://coinlib.io/api/v1/global?key=f2c79808b255e493&pref=EUR")
-.then(function(response) {
-  return response.json();
-})
-.then(function(data) {
-  console.log(data);
-});
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
 
 
 fetch("https://api.exchangerate.host/latest")
-.then(function(response) {
+  .then(function (response) {
     return response.json();
   })
-  .then(function(data2) {
+  .then(function (data2) {
     console.log(data2);
   });
 
-  (function ($) {
-    $(function () {
 
-        //initialize all modals           
-        $('.modal').modal();
-
-        //now you can open modal from code
-        $('#modal1').modal('open');
-
-        //or by click on trigger
-        $('.trigger-modal').modal();
-
-    }); // end of document ready
-})(jQuery); // end of jQuery name space
-  
-  
-  
-  
