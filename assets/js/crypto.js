26b79e3174ee731bf991427241b06e7c6fa29b1b
Alfonso Cerv <pedrocerv1@hotmail.com>
2021-04-28 13:58:34 -0500

    further additions

--- assets/js/crypto.js
+++ assets/js/crypto.js
@@ -1,9 +1,11 @@
 // var userinput = document.queryselect(*input from html that theyre searching*)
 // var desiredrate = document.queryselect(*input from html of the currency they are getting*)
-// this fetch would pull based *userinput* for example btc for eth etc. 
 //document.getElementById(*id of button*).onclick = Submit;
 //document.getElementById(*id of button*).onclick = Submit2;
+// let today = new Date().toLocaleDateString()
 
+
+// this fetch would pull based *userinput* for example btc for eth etc.
 // var Submit = function(){
 fetch("https://coinlib.io/api/v1/coin?key=f2c79808b255e493&symbol=BTC,ETC,DOGE,XMR,TRX")
 .then(function(response) {
@@ -13,8 +15,9 @@ fetch("https://coinlib.io/api/v1/coin?key=f2c79808b255e493&symbol=BTC,ETC,DOGE,X
   console.log(data);
   console.log(data.coins[0].price)
 });
-//return data.coinsp[0].price
+//return data.coins[0].price
 //});
+
 // this fetch would pull based on *userinput* in the from, then their desired conversion in "to=desiredrate"
 // var Submit2 = function(){
 fetch("https://api.exchangerate.host/convert?from=BTC&to=EUR")
@@ -32,5 +35,5 @@ fetch("https://api.exchangerate.host/convert?from=BTC&to=EUR")
 // localStorage.setItem('userinput', data.coins[0]);
 // localStorage.setItem('userinput', data2.result);
 // this will send the local storage to the wishlist, but we will have to make a loop or create a new element per time the funciton is run so it is saving multiple iterations rather than replacing the same one.
-// document.getElementById("*wishlistid*").innerHTML = localStorage.getItem('userinput');
+// document.getElementById("*wishlistid*").innerHTML = userinput + " *currency symbol*"+ localStorage.getItem('userinput'); + " " + today;
 
