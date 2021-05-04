d73a24b1815a5fdbe3a694854a367ff724c34274
Alfonso Cerv <pedrocerv1@hotmail.com>
2021-05-03 22:07:17 -0500

    fixed massive merge error

--- assets/js/crypto.js
+++ assets/js/crypto.js
@@ -1,34 +1,17 @@
-var crypto = document.getElementById("crypto").value;
-var country = document.getElementById("country").value;
-var today = new Date().toLocaleDateString(); // wip
-console.log(today);
 // materialize automatic select css
 $(document).ready(function () {
   $("select").formSelect();
 });
-// fix multiple entries
-
-// add in an event listener that alerts the user they need to select a crypto/country
-// if they select search without choosing an option
-
 
 document.getElementById('cryptoSearchButton').addEventListener('click', function(){
 var crypto = document.getElementById('crypto').value;
 var country = document.getElementById('country').value;
-let today = new Date().toLocaleDateString() // wip
-console.log(today);
-url = ('https://coinlib.io/api/v1/coin?key=f2c79808b255e493&symbol=' + crypto);
-url2 =('https://api.exchangerate.host/convert?from=' + crypto + '&to='+ country);
-console.log(url);
-console.log(url2);
+var today = new Date().toLocaleDateString() 
 
     url =
       "https://coinlib.io/api/v1/coin?key=f2c79808b255e493&symbol=" + crypto;
     url2 =
       "https://api.exchangerate.host/convert?from=" + crypto + "&to=" + country;
-    console.log(url);
-    console.log(url2);
-
     if (crypto == "" || country == "") {
       M.toast({ html: "Please pick a Crypto and/or Country!" });
     } else {
@@ -71,41 +54,6 @@ console.log(url2);
     }
   });
 
-document.getElementById("close").addEventListener("click", function () {
-  document.getElementById("innerModal").innerHTML = "";
-});
-
-document.getElementById('save').addEventListener("click", function() {
-
-//}
-//else{
-fetch(url)
-.then(function(response) {
-  return response.json();
-})
-.then(function(data) {
-  console.log(data);
-  console.log(data.price)
-  localStorage.setItem(crypto, data.price);
-});
-  fetch(url2)
-.then(function(response) {
-    return response.json();
-  })
-  .then(function(data2) {
-    console.log(data2);
-    console.log(data2.result)
-    localStorage.setItem(crypto, data2.result);
-});
-    // these will be under the respective functions for USD and conversion. 
-    // the local storage set above
-    // make list element for creation/populating 
-  var watchlistitem = document.createElement("div").innerHTML = crypto + " " + country + " " + localStorage.getItem(crypto); + " " + today + "<br></br>";
-  console.log(watchlistitem)
-  var innermodal = document.getElementById("innerModal");
-  innermodal.append(watchlistitem);
-});
-
   document.getElementById("close").addEventListener("click", function() {
   document.getElementById('innerModal').innerHTML = "";
 });
@@ -113,9 +61,9 @@ fetch(url)
 document.getElementById("save").addEventListener("click", function() {
 
   var userInput = document.getElementById("innerModal").innerHTML;
-  console.log(userInput);
+  var watchListItem = document.createElement("div").innerHTML = userInput + document.write("\n");
   var savedList = document.getElementById("Watchlist");
-  savedList.append(userInput);
+  savedList.append(watchListItem);
   document.getElementById("innerModal").innerHTML = "";
 });
 
